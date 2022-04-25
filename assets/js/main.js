6b12704367c2c5d05bf2ccd354837ebe60f98ccf
diego <diegoandre.gomez@gmail.com>
2022-04-25 16:36:36 +0200

    updated website

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,13 +290,13 @@
 			$main_articles.each(function() {
 
 				var $this = $(this);
-
 				// Close.
 					$('<div class="close">Close</div>')
 						.appendTo($this)
 						.on('click', function() {
 							location.hash = '';
 						});
+
 
 				// Prevent clicks from inside article from bubbling.
 					$this.on('click', function(event) {
